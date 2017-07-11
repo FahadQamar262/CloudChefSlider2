e17298e0fc4e1692ccc74f40b93462bb3a81acde
FahadQamar262 <fadiqamar99@gmail.com>
2017-07-10 22:43:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/dell/cloudchefslider/splashActivity.java
@@ -0,0 +1,24 @@
+package com.example.dell.cloudchefslider;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+    private static int splash=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(splashActivity.this,MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },splash);
+    }
+}
