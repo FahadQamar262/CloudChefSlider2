7ffa2ef9257a02725e913af68151d7086cc634fb
sumaiya1996 <sumaiya.ubit@gmail.com>
2017-07-13 03:15:53 -0700

    final

---
+++ app/src/main/java/com/example/dell/cloudchefslider/Activity2.java
@@ -0,0 +1,23 @@
+package com.example.dell.cloudchefslider;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    void RegisterOnClick(View v)
+    {
+        Intent myIntent = new Intent(this,Main2Activity.class);
+        startActivity(myIntent);
+
+
+    }
+}
--- app/src/main/java/com/example/dell/cloudchefslider/Main2Activity.java
+++ app/src/main/java/com/example/dell/cloudchefslider/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.dell.cloudchefslider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Spinner mySpinner=(Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Main2Activity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.city));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        Spinner mySpinner1=(Spinner)findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> myAdapter1 = new ArrayAdapter<String>(Main2Activity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.town));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner1.setAdapter(myAdapter1);
     }
 }
